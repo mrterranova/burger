e7776368fca5a508736b278e35fdd274e2facf69
Michal Terranova <michalterranova@mail.com>
2019-11-20 21:44:08 -0500

    burger v.1

--- config/orm.js
+++ config/orm.js
@@ -8,31 +8,20 @@ var orm = {
             console.log(result)
         });
     },
-    allClients: function(clientName, tableName) {
-        var queryString = "SELECT ?? FROM ??";
-        connection.query(queryString, [clientName, tableName], function(err, result){
-            if (err) throw err;
-            console.log("\n------------CLIENT------------")
+    insertOne : function() {
+        var queryString = "INSERT INTO ?? (??, ??) VALUES (?,?)"; 
+        connection.query(queryString, [], function(err, result){
+            if(err) throw err;
             console.log(result)
-        })
+        });
     }, 
-    findGrownUp: function(tableName, colToSearch, valOfCol){
-        var queryString = "SELECT * FROM ?? WHERE ?? = ?";
-        connection.query(queryString, [tableName, colToSearch, valOfCol], function(err, result){
-            if (err) throw err;
-            console.log("\n-----------GROWN_UP---------")
+    updateOne: function() {
+        var queryString = "UPDATE ?? SET ?? = ?, ?? =? WHERE id = ?";
+        connection.query(queryString, [], function(err, result){
+            if(err) throw err; 
             console.log(result)
-        })
-    }, 
-    clientAndParty: function(selection, tableParty, tableClient, onTableOneCall, onTableTwoCall){
-        var queryString = "SELECT ?? FROM ?? AS tOne LEFT JOIN ?? AS tTwo ON tOne.?? = tTwo.??";
-        connection.query(queryString, [selection, tableParty, tableClient, onTableOneCall, onTableTwoCall], function(err, result){
-            if (err) throw err;
-            console.log("\n--------------JOIN----------------")
-            console.log(result)
-        })
+        });
     }
-
 };
 
 module.exports = orm;
--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,10 @@
 ### Schema
 
-CREATE DATABASE parties_db;
+CREATE DATABASE burger_db;
 
-USE parties_db;
+USE burger_db;
 
-CREATE TABLE burgers
+CREATE TABLE burger
 (
 	id int NOT NULL AUTO_INCREMENT,
 	burger_name varchar(50) NOT NULL,
