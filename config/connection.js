c6e7a9951bbaa699260b6d315a744bc3ca7309ac
Michal Terranova <michalterranova@mail.com>
2019-11-26 14:35:52 -0500

    dependencies changed

--- config/connection.js
+++ config/connection.js
@@ -3,9 +3,9 @@ var mysql = require("mysql");
 var connection = mysql.createConnection({
   host: "bmsyhziszmhf61g1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
-  user: "	z80oqju65ovyua5z",
+  user: "z80oqju65ovyua5z",
   password: "s8du9viktu1rt4ey",
-  database: "	lybvyfhs4itkua2j"
+  database: "lybvyfhs4itkua2j"
 });
 
 connection.connect(function(err) {
